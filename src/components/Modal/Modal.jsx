8e766549ee3c48818595b211fb20562a5670fa11
SzymonSleboda <szymonskeboda@gmail.com>
2023-10-24 21:46:55 +0200

    feature/database-update

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,26 +1,27 @@
 import React from "react";
 import s from "./Modal.module.css";
 import Icon from "../../data/icons.svg";
-import Data from "../../data/labelCategories";
 
-const Modal = ({ handleOpen, handleClose, isOpen }) => {
-  const handleIsOpen = () => {
-    handleOpen();
-  };
-  const handleIsClose = () => {
-    handleClose();
-  };
-
+const Modal = ({
+  handleOpen,
+  handleClose,
+  isOpen,
+  handleFood,
+  handleFunTimeDay,
+  handleFunTimeEvening,
+  handleRestaurants,
+  handleSurprise,
+}) => {
   return (
     <div>
-      {isOpen && <div className={s.overlay} onClick={handleIsClose}></div>}
-      <button className={s.menu_open} onClick={handleIsOpen}>
+      {isOpen && <div className={s.overlay} onClick={handleClose}></div>}
+      <button className={s.menu_open} onClick={handleOpen}>
         <svg className={`${s.svg_open} ${isOpen ? s.is_hidden : ""}`}>
           <use href={Icon + "#icon-burger-icon"}></use>
         </svg>
       </button>
       <div className={`${s.menu_container} ${isOpen ? s.is_open : ""}`}>
-        <button className={s.menu_close} onClick={handleIsClose}>
+        <button className={s.menu_close} onClick={handleClose}>
           <svg className={s.svg_close}>
             <use href={Icon + "#icon-close-icon"}></use>
           </svg>
@@ -30,11 +31,11 @@ const Modal = ({ handleOpen, handleClose, isOpen }) => {
           <li>
             <h2>Categories:</h2>
           </li>
-          <li>Option 1</li>
-          <li>Option 2</li>
-          <li>Option 3</li>
-          <li>Option 4</li>
-          <li>Option 5</li>
+          <li onClick={handleFood}>Food</li>
+          <li onClick={handleFunTimeDay}>Fun Time Day</li>
+          <li onClick={handleFunTimeEvening}>Fun Time Evening</li>
+          <li onClick={handleRestaurants}>Restaurants</li>
+          <li onClick={handleSurprise}>Surprise Me!</li>
         </ul>
       </div>
     </div>
